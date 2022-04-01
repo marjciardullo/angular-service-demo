b0d35ea5391ad8ae8ec62264359075cf83d7237b
marjciardullo <marjoryeramirez@gmail.com>
2022-04-01 20:36:35 -0300

    2 files modified, 6 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,28 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { DisciplinasComponent } from './disciplinas/disciplinas.component';
+import { TemporizadorComponent } from './temporizador/temporizador.component';
+import { RouterModule } from '@angular/router';
+import { LogComponent } from './log/log.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      { path: '', component: TemporizadorComponent },
+      { path: 'disciplinas', component: DisciplinasComponent },
+      { path: 'log', component: LogComponent },
+    ]),
+  ],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    DisciplinasComponent,
+    TemporizadorComponent,
+    LogComponent,
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
